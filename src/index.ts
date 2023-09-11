7e9e280a57f03d938c7b6ffc31efebdee26cb150
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-09-11 18:33:49 +0200

    Add dbConnect function in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
+import createDebug from 'debug';
+import 'dotenv/config';
+import { createServer } from 'http';
+import { app } from './app.js';
+import { dbConnect } from './dbConnect/dbConnect.js';
 
+const PORT = process.env.PORT || 3000;
+const debug = createDebug('PF11:Index');
+const server = createServer(app);
+
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('Connected to DataBase: ', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => {
+    server.emit('error', error);
+  });
+
+server.on('listening', () => {
+  debug(`Listening on port ${PORT}`);
+});
+
+server.on('error', (error) => {
+  debug(`Error ${error.message}`);
+});
